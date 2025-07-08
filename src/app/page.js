401645b490df393d17d7ab048f6a8105333e1163
Isha Chaudhary <chaudharyisha900@gmail.com>
2025-07-07 21:42:25 -0700

    created branch

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    <p className="bg-green-300">Altaf Raza Siddique</p>
+    <p >Altaf Raza Siddique</p>
    </div>
   );
 }
